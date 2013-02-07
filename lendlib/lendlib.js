ec6e7f065f13e7e437996993957c3800dfc70cb1
Arafat Mohamed <github@arafatm.com>
2013-02-07 17:16:27 -0600

    code cleanup: remove "hello world"

--- lendlib/lendlib.js
+++ lendlib/lendlib.js
@@ -1,18 +1,6 @@
 var lists = new Meteor.Collection("Lists");
 
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "My List.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-
   Template.categories.lists = function () {
     return lists.find({}, {sort: {Category: 1}});
   };
